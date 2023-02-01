c32b252e6261541ec4480d61fea09d62bc24ee4d
belbafolder <3215903@kabk.nl>
2023-02-01 11:16:17 +0100

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+document.querySelector(".saltimage").addEventListener("click", function () {
+    document.querySelector(".abstract").style.visibility="visible";
+
+    console.log("clicked image");
+    console.log(document.querySelector(".abstract").style.visibilty);
+});
+
+// console.log(document.querySelector(".abstract").innerHTML);
+
+});
+
